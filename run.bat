fa231b0e58721494b6126b72f314bd113bcaf18f
Jianwen Wei <wei.jianwen@gmail.com>
2011-08-18 21:07:14 +0800

    Add starting scripts run.sh/run.bat. Modify the mongodb lib Path to a relative path.

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -classpath ./bin;./lib/mongo-2.6.3.jar cn.edu.sjtu.syslogmapreduce.SyslogMapReduce
+pause
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -classpath ./bin:./lib/mongo-2.6.3.jar cn.edu.sjtu.syslogmapreduce.SyslogMapReduce
